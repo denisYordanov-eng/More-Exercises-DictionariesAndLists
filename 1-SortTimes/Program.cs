4030ed6a095d1d41be6bb20f29e7621f2509d88b
denisYordanov-eng <denisivov437@gmail.com>
2025-04-22 10:33:34 +0300

    Made a task.

---
+++ 1-SortTimes/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1_SortTimes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var times = Console.ReadLine().Split(' ').ToList();
+
+            var sortedTimes = times.OrderBy(a => a).ToArray();
+            Console.WriteLine(String.Join(", ", sortedTimes));
+        }
+    }
+}
