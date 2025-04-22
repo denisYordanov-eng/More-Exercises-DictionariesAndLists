fb44d6b5d11e4ccc9570681de4f0cc1b2a026cbd
denisYordanov-eng <denisivov437@gmail.com>
2025-04-22 11:05:27 +0300

    Made a task.

---
+++ 02-OddFilter/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _02_OddFilter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbersInput = Console.ReadLine()
+                .Split(' ')
+                .Select(int.Parse)
+                .ToArray();
+            int[] evenNumber =
+                numbersInput.Where(a => a % 2 == 0).ToArray();
+
+            int[] finalArr = evenNumber
+                .Select(a => a >= evenNumber.Average() ? ++a : --a)
+                .ToArray();
+
+            Console.WriteLine(String.Join(" ", finalArr));
+           
+        }
+    }
+}
