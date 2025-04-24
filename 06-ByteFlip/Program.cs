7b91ebdbe6ee89669758f7a4e7ca3022d9cb6bf1
denisYordanov-eng <denisivov437@gmail.com>
2025-04-24 19:17:43 +0300

    Made a task.

---
+++ 06-ByteFlip/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.Design;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _06_ByteFlip
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] input = Console.ReadLine().Split(' ').ToArray();
+
+            var filterArr = input.Where(a => a.Length == 2)
+                .Select(a => new string(a.Reverse().ToArray()))
+                .Reverse()
+                .ToList();
+
+            string result = string.Concat(filterArr.Select(a =>(char) Convert.ToInt32(a, 16)));
+
+            Console.WriteLine(result);
+        }
+    }
+}
